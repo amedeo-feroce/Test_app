47f365e162d120d44b6ee86c3d30ef2b753e2688
Amedeo, Feroce <amedeo.feroce@accenture.com>
2019-09-18 10:57:27 +0200

    Create Entity Derby

---
+++ src/main/java/com/acn/nemo/model/Utente.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acn.nemo.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author amedeo.feroce
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "UTENTE", catalog = "", schema = "AMEDEO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Utente.findAll", query = "SELECT u FROM Utente u")
+    , @NamedQuery(name = "Utente.findById", query = "SELECT u FROM Utente u WHERE u.id = :id")
+    , @NamedQuery(name = "Utente.findByPassword", query = "SELECT u FROM Utente u WHERE u.password = :password")
+    , @NamedQuery(name = "Utente.findByUsername", query = "SELECT u FROM Utente u WHERE u.username = :username")})
+public class Utente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Size(max = 255)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Size(max = 255)
+    @Column(name = "USERNAME")
+    private String username;
+
+    
+}
